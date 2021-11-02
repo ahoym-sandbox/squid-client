525a2a9401f5a1c0af6e9d3d1273b61bb54c5697
Florent Bariod <fbariod@ripple.com>
2021-11-02 16:24:33 -0400

    just using one wallet for the client

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import logo from './logo.svg';
-import { xrplClient, xrplClientTwo } from './XrplApiSandbox';
+import { useEffect, useState } from "react";
+import "./App.css";
+import logo from "./logo.svg";
+import { xrplClient, xrplClientTwo } from "./XrplApiSandbox";
 
 // Can import and run TS scripts this way if so desired
 // import './XrplApiSandbox/scripts/sendXrp';
@@ -9,7 +9,7 @@ import { xrplClient, xrplClientTwo } from './XrplApiSandbox';
 
 // Generate testnet wallets
 const generateWalletRequestOne = xrplClient.generateFaucetWallet();
-const generateWalletRequestTwo = xrplClientTwo.generateFaucetWallet();
+// const generateWalletRequestTwo = xrplClientTwo.generateFaucetWallet();
 
 function App() {
   const [logs, setLogs] = useState<unknown[]>([]);
@@ -18,36 +18,36 @@ function App() {
     generateWalletRequestOne.then((result) => {
       setLogs((logState) => [
         result,
-        'Created faucet wallet for Client 1',
+        "Created faucet wallet for Client 1",
         ...logState,
       ]);
     });
   }, []);
 
-  useEffect(() => {
-    generateWalletRequestTwo.then((result) => {
-      setLogs((logState) => [
-        result,
-        'Created faucet wallet for Client 2',
-        ...logState,
-      ]);
-    });
-  }, []);
+  // useEffect(() => {
+  //   generateWalletRequestTwo.then((result) => {
+  //     setLogs((logState) => [
+  //       result,
+  //       'Created faucet wallet for Client 2',
+  //       ...logState,
+  //     ]);
+  //   });
+  // }, []);
 
-  useEffect(() => {
-    // After testnet wallet creations, send a 22 XRP payment
-    Promise.all([generateWalletRequestOne, generateWalletRequestTwo])
-      .then(() =>
-        xrplClient.sendPayment(22, xrplClientTwo.wallet()?.account.address!)
-      )
-      .then((result) => {
-        setLogs((logState) => [
-          result,
-          'Sent transaction from Wallet 1 to Wallet 2',
-          ...logState,
-        ]);
-      });
-  }, []);
+  // useEffect(() => {
+  //   // After testnet wallet creations, send a 22 XRP payment
+  //   Promise.all([generateWalletRequestOne, generateWalletRequestTwo])
+  //     .then(() =>
+  //       xrplClient.sendPayment(22, xrplClientTwo.wallet()?.account.address!)
+  //     )
+  //     .then((result) => {
+  //       setLogs((logState) => [
+  //         result,
+  //         'Sent transaction from Wallet 1 to Wallet 2',
+  //         ...logState,
+  //       ]);
+  //     });
+  // }, []);
 
   return (
     <div className="App">
@@ -56,13 +56,13 @@ function App() {
 
         <div className="App-logs">
           {logs.map((log) => {
-            if (typeof log === 'string') {
+            if (typeof log === "string") {
               return (
                 <p key={Math.random()} className="App-console-log">
                   {log}
                 </p>
               );
-            } else if (typeof log === 'object') {
+            } else if (typeof log === "object") {
               return (
                 <div key={Math.random()}>
                   <pre>{JSON.stringify(log, null, 2)}</pre>
