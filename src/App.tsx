83cf1c05a4ac026c451ca5d7f1fe9d429aa60236
Malcolm Ahoy <mahoy@ripple.com>
2021-11-03 19:32:07 -0400

    Squid

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import { useState } from "react";
-import "./App.css";
-import logo from "./logo.svg";
-import { xrplClient } from "./XrplApiSandbox";
+import { useState } from 'react';
+import './App.css';
+import logo from './logo.svg';
+import { Square } from './shapes/Square';
+import { Circle } from './shapes/Circle';
+import { Triangle } from './shapes/Triangle';
+import { xrplClient } from './XrplApiSandbox';
 
 // Can import and run TS scripts this way if so desired
 // import './XrplApiSandbox/scripts/sendXrp';
@@ -11,8 +14,8 @@ import { xrplClient } from "./XrplApiSandbox";
 // const bankWallet = "rUEqxgBLfgoqZWC8B94shLXUV8pUxhwrnX";
 // const oracleWallet = "rDDqrVxbVgyxkit5jEd84ndwi1YpxGqgL7";
 
-const bankWallet = "rpMzbkZuxApNHJTAETbDB9e68b9XC9CY2C";
-const oracleWallet = "rgbv6kNj77J9DTjZM39Q5xi1pzufv13g1";
+const bankWallet = 'rpMzbkZuxApNHJTAETbDB9e68b9XC9CY2C';
+const oracleWallet = 'rgbv6kNj77J9DTjZM39Q5xi1pzufv13g1';
 var playerWallet: any;
 
 // game state (=== escrow state)
@@ -25,7 +28,7 @@ var walletCreated = xrplClient.generateFaucetWallet();
 
 // send a payment
 var paymentSent = walletCreated.then((result) => {
-  console.log("wallet created");
+  console.log('wallet created');
   console.log(result);
   playerWallet = result?.account?.address;
   return xrplClient.sendPayment(1, bankWallet);
@@ -33,17 +36,17 @@ var paymentSent = walletCreated.then((result) => {
 
 // listen for account sets
 paymentSent.then((result) => {
-  console.log("payment sent");
+  console.log('payment sent');
   console.log(result);
   return xrplClient.subscribeToAccountTransactions(
     {
       accounts: [oracleWallet],
     },
     (event: any) => {
-      if ("AccountSet" === event["transaction"].TransactionType) {
-        console.log("account set event received");
+      if ('AccountSet' === event['transaction'].TransactionType) {
+        console.log('account set event received');
         console.log(event);
-        decodeMemo(event["transaction"].Memos);
+        decodeMemo(event['transaction'].Memos);
       }
       return Promise.resolve(event);
     }
@@ -57,10 +60,10 @@ paymentSent.then((result) => {
       accounts: [playerWallet],
     },
     (event: any) => {
-      if ("EscrowCreate" === event["transaction"].TransactionType) {
-        console.log("escrow create event received");
+      if ('EscrowCreate' === event['transaction'].TransactionType) {
+        console.log('escrow create event received');
         console.log(event);
-        escrowOfferSequence = event["transaction"].Sequence;
+        escrowOfferSequence = event['transaction'].Sequence;
       }
       return Promise.resolve(event);
     }
@@ -68,23 +71,27 @@ paymentSent.then((result) => {
 });
 
 function App() {
-  console.log("App called");
+  console.log('App called');
   const [logs, setLogs] = useState<unknown[]>([]);
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div className="Squid">
+          <Circle />
+          <Triangle />
+          <Square />
+        </div>
 
         <div className="App-logs">
           {logs.map((log) => {
-            if (typeof log === "string") {
+            if (typeof log === 'string') {
               return (
                 <p key={Math.random()} className="App-console-log">
                   {log}
                 </p>
               );
-            } else if (typeof log === "object") {
+            } else if (typeof log === 'object') {
               return (
                 <div key={Math.random()}>
                   <pre>{JSON.stringify(log, null, 2)}</pre>
@@ -114,31 +121,31 @@ function App() {
 function decodeMemo(memo: any[]) {
   memo.forEach((m, idx) => {
     var hexValue = m.Memo.MemoData.toString();
-    var value = "";
+    var value = '';
     for (var n = 0; n < hexValue.length; n += 2) {
       value += String.fromCharCode(parseInt(hexValue.substr(n, 2), 16));
     }
 
     var hexType = m.Memo.MemoType.toString();
-    var type = "";
+    var type = '';
     for (var n = 0; n < hexType.length; n += 2) {
       type += String.fromCharCode(parseInt(hexType.substr(n, 2), 16));
     }
 
-    if (type === "nft/1") {
+    if (type === 'nft/1') {
       escrowCondition = value;
       console.log("you're in the squid game!");
     }
-    if (type === "nft/2") {
+    if (type === 'nft/2') {
       escrowFulfilment = value;
-      console.log("you won the squid game!");
+      console.log('you won the squid game!');
       finishEscrow();
     }
   });
 }
 
 function finishEscrow() {
-  console.log("finishing escrow");
+  console.log('finishing escrow');
   console.log([
     bankWallet,
     escrowOfferSequence,
